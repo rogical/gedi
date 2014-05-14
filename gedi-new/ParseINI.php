85e5fa7885e5a3c2ffb600388cdd3c0a7742e07f
patrick <chengyi.wu@capgemini.com>
2014-05-14 15:17:35 +0800

    add parse ini and xml file

---
+++ gedi-new/ParseINI.php
@@ -0,0 +1,100 @@
+<?php
+/**
+ * ParseINI class
+ *
+ * This class represents an INI configuation file.
+ * @author Patrick Wu
+ * @package gedi
+ * @version 1.0
+ */
+Class ParseINI{
+	/**
+     * The array of the GEDI INI.
+     *
+     * @since 1.0
+     * @var array
+     * @access protected
+     */
+	protected  $ini_array = array();
+	/**
+     * The mapping array of the GEDI INI.
+     *
+     * @since 1.0
+     * @var array
+     * @access protected
+     */
+	protected  $mapping_ini = array();
+    /**
+     * The $key of the GEDI INI array.
+     *
+     * @since 1.0
+     * @var string
+     * @access protected
+     */
+	protected  $key = "";
+	 /**
+     * Class constructor
+     *
+     * This function is used to instantiate the class. The instatiation is done
+     * <br/>Example:
+     * <code>
+     * $ini = new ParseINI();              
+     * </code>
+     * @since 1.0
+     * @access public
+     */
+    function __construct(){
+	    $this->ini_array = parse_ini_file("gedi.ini");
+	    if($this->ini_array){
+			$this->mapping_ini['-gediip'] = $this->ini_array['GEDI_IP'];
+			$this->mapping_ini['-cn'] = $this->ini_array['CN'];
+			$this->mapping_ini['-pass'] = $this->ini_array['PASSWD'];
+			$ftp_info_str = "";
+			if($this->ini_array['FTP_IP']){
+				$ftp_info_str.= $this->ini_array['FTP_IP'];
+				if($this->ini_array['FTP_USER'] && $this->ini_array['FTP_PASSWD']){
+					$ftp_info_str.=",".$this->ini_array['FTP_USER'].",".$this->ini_array['FTP_PASSWD'];
+					if($this->ini_array['FTP_SUBDIR']){
+						$ftp_info_str.=",".$this->ini_array['FTP_SUBDIR'];
+					}
+				}
+			}
+			$this->mapping_ini['-ftp'] = $ftp_info_str;
+			$this->mapping_ini['PROXY_IP'] = $this->ini_array['PROXY_IP'];
+			$this->mapping_ini['PROXY_PORT'] = $this->ini_array['PROXY_PORT'];
+			$this->mapping_ini['CONFIG_FILE'] = $this->ini_array['CONFIG_FILE'];
+			$this->mapping_ini['GEDINIPATH'] = $this->ini_array['GEDINIPATH'];
+			
+	    }
+	}
+	 /**
+     * Function get()
+     *
+     * This function is used to get ini array or key value
+     * <br/>Example:
+     * <code>
+     * $ini = new ParseINI();    
+     * $ret = $ini->get();//return all array 
+	 * $ret = $ini->get('-gediip');//return "-gediip" value
+     * </code>
+     *
+     * @return string or Array
+     * @since 1.0
+     * @access public
+     */
+	public function get($key=''){
+	    if($key){
+			return $this->mapping_ini[$key];
+	    } else {
+			return $this->mapping_ini;
+	    }
+	}
+}
+/*
+$ini = new ParseINI();
+echo "<pre>";
+var_dump($ini->get());
+var_dump($ini->get('-gediip'));
+echo "</pre>";
+*/
+?>
---
+++ gedi-new/ParseXML.php
@@ -0,0 +1,81 @@
+<?php 
+/**
+ * XML class
+ *
+ * This class represents an XML configuation file.
+ * @author Patrick WU
+ * @package gedi
+ * @version 1.0
+ */
+Class ParseXML{
+	/**
+     * The array of the GEDI configuation xml file.
+     *
+     * @since 1.0
+     * @var array
+     * @access protected
+     */
+	protected $xml_array = array();
+	 /**
+     * Class constructor
+     *
+     * This function is used to instantiate the class. The instatiation is done
+     * <br/>Example:
+     * <code>
+     * $xml = new ParseXML();              
+     * </code>
+     * @since 1.0
+     * @access public
+     */
+	function __construct(){
+		$xml = simplexml_load_file("configparam.xml");
+		$res = $xml->xpath("param");
+
+		$this->xml_array = array();
+
+		if($res){
+			foreach($res as $key=>$val){
+				$xml_array_key  =  str_replace("</name>","",str_replace("<name>","",$val->name->asXML()));
+				if($xml_array_key) {
+					$this->xml_array["$xml_array_key"]["def"] = str_replace("</def>","",str_replace("<def>","",$val->def->asXML()));
+					$this->xml_array["$xml_array_key"]["balise"] = str_replace("</balise>","",str_replace("<balise>","",$val->balise->asXML()));
+				}
+			}
+		}
+	}
+	/**
+     * Function get()
+     *
+     * This function is used to get ini array or key value
+     * <br/>Example:
+     * <code>
+     * $xml = new ParseXML();    
+     * $ret = $xml->get('reference');//return the array of  key 'reference' value 'def' and 'balise'
+	 * $ret = $xml->get('reference','def');//return the key 'reference' value of "def"
+     * </code>
+     *
+     * @return mixed string or Array
+     * @since 1.0
+     * @access public
+     */
+	public function get($key='',$name=''){
+	   if($key){
+			if($name){
+				return $this->xml_array[$key][$name];
+			}else{
+				return $this->xml_array[$key];
+			}
+	   }else{
+			return "";
+	   }
+	}
+
+}
+/*
+$xml = new ParseXML();
+echo "<pre>";
+var_dump($xml->get('reference'));
+var_dump($xml->get('reference','def'));
+echo "</pre>";
+*/
+?>
