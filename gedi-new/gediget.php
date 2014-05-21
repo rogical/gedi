2170dec7369fc39bb438131f3dc854ba8a3ac414
winnwang <aiyou222@126.com>
2014-05-21 15:41:17 +0800

    Update gediget.php

--- gedi-new/gediget.php
+++ gedi-new/gediget.php
@@ -1,44 +1,37 @@
 <?php
 require_once './lib/ParseINI.php';
 $gediPath = $argv[1];
+unset($argv[0]);
+unset($argv[1]);
 //php request api file name
 $phpFile = $argv[2];
+unset($argv[2]);
 //document name
 $parameters['file'] = isset($argv[3])?$argv[3]:'';
+unset($argv[3]);
 //language
 $parameters['language'] = isset($argv[4])?$argv[4]:'';
+unset($argv[4]);
 //edition
 if(!empty($argv[5])){
-	if(substr($argv[5],0,1) !== '-')
+	if(substr($argv[5],0,1) !== '-'){
 		$parameters['edition'] = $argv[5];
+		unset($argv[5]);
+	}
 }
+$userInfo = array('-gediip','-cn','-pass');
 foreach($argv as $k=>$arg){
-	if(substr($arg,0,1) == '-'){
-		if(isset($argv[$k+1])){
-			if(substr($argv[$k+1],0,1) == '-'){
-				$parameters[$arg] = $arg;
-			}else{
-				$parameters[$arg] = $argv[$k+1];
-			}
+	if(in_array($arg,$userInfo)){
+		if(isset($argv[$k+1]) && !in_array($argv[$k+1],$userInfo)){
+			$parameters[$arg] = $argv[$k+1];
 		}else{
+			$parameters[$arg] = '';
+		}
+	}else{
+		if(!in_array($argv[$k-1],$userInfo)){
 			$parameters[$arg] = $arg;
 		}
 	}
-}
-
-$parseINIObj = new ParseINI();
-//check ip
-if(!array_key_exists('-gediip',$parameters)){
-	//get gedi ip from gedi.ini
-	$parameters['-gediip'] = $parseINIObj->get('-gediip');
-}
-//check username
-if(!array_key_exists('-cn',$parameters)){
-	$parameters['-cn'] = $parseINIObj->get('-cn');
-}
-//check pass
-if(!array_key_exists('-pass',$parameters)){
-	$parameters['-pass'] = $parseINIObj->get('-pass');
 }
 
 //parameters error message
@@ -63,6 +56,10 @@ Options are :
  -nosrc                       Do not transfer the document
 ';
 
+if(array_key_exists('-cn',$parameters) && array_key_exists('-pass',$parameters) && empty($parameters['-cn']) && !empty($parameters['-pass'])){
+	echo $parameters_error;
+	exit();
+}
 //file name
 if(empty($parameters['file']) || substr($parameters['file'],0,1) == '-'){
 	echo $parameters_error;
@@ -81,6 +78,22 @@ foreach($parameters as $key=>$value){
 		exit();
 	}
 }
+
+$parseINIObj = new ParseINI();
+//check ip
+if(!array_key_exists('-gediip',$parameters)){
+	//get gedi ip from gedi.ini
+	$parameters['-gediip'] = $parseINIObj->get('-gediip');
+}
+//check username
+if(!array_key_exists('-cn',$parameters)){
+	$parameters['-cn'] = $parseINIObj->get('-cn');
+}
+//check pass
+if(!array_key_exists('-pass',$parameters)){
+	$parameters['-pass'] = $parseINIObj->get('-pass');
+}
+//print_r($parameters);
 
 //do other options
 require_once $gediPath.'/'.$phpFile;
