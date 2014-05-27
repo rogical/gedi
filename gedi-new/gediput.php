75440757c0854496e95ae01fe4b97b69d0d4c350
winnwang <aiyou222@126.com>
2014-05-27 13:47:01 +0800

    the gediput.php file use to parse paramters

---
+++ gedi-new/gediput.php
@@ -0,0 +1,37 @@
+<?php
+require_once './lib/ParseINI.php';
+$gediPath = $argv[1];
+unset($argv[0]);
+unset($argv[1]);
+//php request api file name
+$phpFile = $argv[2];
+unset($argv[2]);
+//document name
+$parameters['file'] = isset($argv[3])?$argv[3]:'';
+unset($argv[3]);
+
+foreach($argv as $k=>$arg){
+	if(substr($arg,0,1) == '-'){
+		$parameters[$arg] = @$argv[$k+1];
+	}
+	
+}
+
+$parseINIObj = new ParseINI();
+//check ip
+if(!array_key_exists('-gediip',$parameters)){
+	//get gedi ip from gedi.ini
+	$parameters['-gediip'] = $parseINIObj->get('-gediip');
+}
+//check username
+if(!array_key_exists('-cn',$parameters)){
+	$parameters['-cn'] = $parseINIObj->get('-cn');
+}
+//check pass
+if(!array_key_exists('-pass',$parameters)){
+	$parameters['-pass'] = $parseINIObj->get('-pass');
+}
+print_r($parameters);
+
+//do other options
+require_once $gediPath.'/'.$phpFile;
